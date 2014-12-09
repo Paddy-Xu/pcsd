cbaffe3bcad2cc5a1e9797a9cdd30f428c969a20
Johannes de Fine Licht <johannes.definelicht@cern.ch>
2014-12-09 23:24:22 +0100

    Modified colors and added legends

--- assignment3/acertainbookstore/plot.r
+++ assignment3/acertainbookstore/plot.r
@@ -6,13 +6,17 @@ throughput = c(local$throughput/max(local$throughput),
 plot(0, 0, xlim=c(0, 11), ylim=c(min(throughput), 1), type="n",
      xlab="# of worker threads [1]",
      ylab="Normalized throughput [Successful interactions/second]")
-lines(local$workers, local$throughput/max(local$throughput), type="b", col="firebrick", pch=3)
-lines(server$workers, server$throughput/max(server$throughput), type="b", col="royalblue", pch=4)
+lines(local$workers, local$throughput/max(local$throughput), type="b", col="red", pch=3)
+lines(server$workers, server$throughput/max(server$throughput), type="b", col="blue", pch=4)
+legend("right", c("Local", "Server"), pch=c(3, 4), lty=c(1,1),
+       col=c("red", "blue"))
 pdf("latency.pdf")
 latency = c(local$latency/max(local$latecy),
             server$latency/max(server$latemcy))
 plot(0, 0, xlim=c(0, 11), ylim=c(min(latency), 1), type="n",
      xlab="# of worker threads [1]",
      ylab="Normalized latency [Seconds/successful interaction/worker]")
-lines(local$workers, local$latency/max(local$latency), type="b", col="forestgreen", pch=3)
-lines(server$workers, server$latency/max(server$latency), type="b", col="tomato", pch=4)
+lines(local$workers, local$latency/max(local$latency), type="b", col="red", pch=3)
+lines(server$workers, server$latency/max(server$latency), type="b", col="blue", pch=4)
+legend("topleft", c("Local", "Server"), pch=c(3, 4), lty=c(1, 1),
+       col=c("red", "blue"))
